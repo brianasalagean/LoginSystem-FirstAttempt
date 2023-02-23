3806667a641098393f4b6effefdbb09d56ae8d44
brianasalagean <briana.salagean@stud.ubbcluj.ro>
2023-02-23 22:07:58 +0200

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ASPLoginSystem.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" href="site.css" />
+</head>
+<body>
+    <h1>Welcome</h1>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+            <asp:Label ID="usernamelbl" runat="server" Text="User Name"></asp:Label>
+            </div>
+            <div>
+            <asp:TextBox ID="username" runat="server"></asp:TextBox>
+            </div>
+            <asp:Label ID="passwdlbl" runat="server" Text="Password"></asp:Label>
+            <div>
+            <asp:TextBox ID="passwd" runat="server" TextMode="Password"></asp:TextBox>
+            </div>
+            <div class="hiddenlbl">
+            <asp:Label ID="confirmationlbl" runat="server" Text="Confirmation" Visible="false"></asp:Label>
+            </div>
+            <div>
+            <asp:Button ID="loginButton" runat="server" OnClick="loginClick" Text="Login" />
+            <asp:Button ID="resetButton" runat="server" OnClick="resetClick" Text="Reset" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPLoginSystem
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection SqlConn = new SqlConnection("Data Source=DESKTOP-7C3479M\\SQLEXPRESS; Initial Catalog=ASPLogin; Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loginClick(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM UserAcc WHERE UserName='" +  username.Text + "' AND Password='" + passwd.Text + "'", SqlConn);  
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+
+            if (dataTable.Rows.Count > 0)
+            {
+                confirmationlbl.Text = "You've successfully logged into the system!";
+                confirmationlbl.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                confirmationlbl.Text = "Invalid UserName or Password!";
+                confirmationlbl.ForeColor = System.Drawing.Color.Red;
+            }
+            confirmationlbl.Visible = true;
+        }
+
+        protected void resetClick(object sender, EventArgs e)
+        {
+            username.Text = "";
+            passwd.Text = "";
+            confirmationlbl.Visible = false;
+            confirmationlbl.ForeColor = System.Drawing.Color.Green;
+        }
+    }
+}
